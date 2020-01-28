d080bd56cf8a83770e1764a13f916df275ec4aae
sudeepgumaste <sudeepgumaste19@gmail.com>
2020-01-28 21:21:10 +0530

    Added test route

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import process from "process";
 import { router } from "./router";
 import mongoose from "mongoose";
-import cors from 'cors';
+import cors from "cors";
 
 //configuring environment
 dotenv.config();
@@ -12,21 +12,26 @@ dotenv.config();
 const app = express();
 
 //middlewares
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(router);
+
+app.get("/", (req, res) => {
+  // console.log(req);
+  res.send("<h1>It works</h1>");
+});
 
 //db connection
 mongoose.connect(
   process.env.DB_URI,
-  {  
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify : false
+    useFindAndModify: false
   },
   () => {
     console.log("Connected to DB");
-  },
+  }
 );
 
 const PORT = process.env.PORT || 5000;
